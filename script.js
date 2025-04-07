d153dd740703c86be44ec168d69f06f331ff6d0f
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-07 17:14:33 +0200

    [UPD] creo funzione genera card e funzione che inserisce le card. Milestone 3 done

--- script.js
+++ script.js
@@ -1,66 +1,44 @@
-// const location = [
-//     {
-//         "id": 1,
-//         "title": "Skate Park",
-//         "date": "01-07-2024",
-//         "url": "https://marcolanci.it/boolean/assets/pictures/1.png"
-//     },
-//     {
-//         "id": 2,
-//         "title": "Passeggiata",
-//         "date": "16-07-2024",
-//         "url": "https://marcolanci.it/boolean/assets/pictures/2.png"
-//     },
-//     {
-//         "id": 3,
-//         "title": "Alpi",
-//         "date": "01-07-2024",
-//         "url": "https://marcolanci.it/boolean/assets/pictures/3.png"
-//     },
-//     {
-//         "id": 4,
-//         "title": "Sagra",
-//         "date": "21-08-2024",
-//         "url": "https://marcolanci.it/boolean/assets/pictures/4.png"
-//     },
-//     {
-//         "id": 5,
-//         "title": "Watergun",
-//         "date": "23-08-2024",
-//         "url": "https://marcolanci.it/boolean/assets/pictures/5.png"
-//     },
-//     {
-//         "id": 6,
-//         "title": "Riviera",
-//         "date": "30-08-2024",
-//         "url": "https://marcolanci.it/boolean/assets/pictures/6.png"
-//     }
-// ]
-
-// richiamo gli elementi dom necessari
-
-// funzione per generare le card
-
-// funzione che crea le card
-// `                    <!-- <div class="col-12 col-md-6 col-lg-4 mt-5 colonna" style="border:1px solid #0a0313;">
-//                         <div class="picSquare">
-//                             <figure>
-//                                 <img src="./img/provaUnreal.jpg" alt="">
-//                                 <img src="./img/pin.svg" class="point" alt="puntina">
-//                             </figure>
-//                             <figcaption>
-//                                lotumdmomsdocmscmsocsmosom
-//                             </figcaption>
-//                         </div>
-//                     </div> -->`
-
 // dischiaro const che conterrà le chiavi valore della chiamata api
 const newGroup = []
 
-// effettuo chiamata AudioParam, estrapolando il contenuto nell array newGropu
-axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then((resp) =>{
-    resp.data.map(item => {
-        newGroup.push(item)
-    });
-})
-console.log(newGroup);
+
+// creo la mia funzione per destrutturare e creare una singola card
+const generateCard = (element) => {
+    // creo una stringa dove inserire la mia card
+    const card = `<div class="col-12 col-md-6 col-lg-4 mt-5 colonna" style="border:1px solid #0a0313;">
+                        <div class="picSquare">
+                        <figure>
+                        <img src="${element.url}" talt="">
+                        <img src="./img/pin.svg" class="point" alt="puntina">
+                        </figure>
+                            <figcaption>
+                            ${element.date}
+                            ${element.title}
+                            </figcaption>
+                        </div>
+                    </div>`
+                        
+            return card;
+    }
+
+// funzione che mi renderizza il contenuto dell'array
+const render = (array) => {
+    // dichiaro una variabile con valore stringa nuova concatenando le colonne come stringhe
+    let cards = "";
+    for(let i=0; i<array.length; i++){
+      cards += generateCard(array[i])
+    }
+  
+    document.getElementById(`content`).innerHTML = cards;
+  }
+  
+  // chiamo la funzione
+  
+  // effettuo chiamata AudioParam, estrapolando il contenuto nell array newGropu
+  axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then((resp) =>{
+      resp.data.map(item => {
+          newGroup.push(item)
+          render(newGroup);
+      });
+  })
+  console.log(newGroup);
