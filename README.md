70e5530bbbecd9fc1cc7953d5f12e14d086e5ec0
mauroanastasi <mauroanastasi1@gmail.com>
2025-04-08 16:25:40 +0200

    [UPD] funzione per trovare img cliccata. MIL 3 done

--- README.md
+++ README.md
@@ -19,3 +19,15 @@ rendi la pagina responsive, in modo che su mobile e tablet le foto si dispongano
 Note
 Non siete obbligati a usare Bootstrap: siete liberi di decidere come gestire lo stile :faccia_leggermente_sorridente:
 Buon Lavoro e buon divertimento!
+--------------------------------------------------------------------------
+:roccia: MILESTONE 1
+Facciamo in modo di creare un overlay che copra l’intera pagina e all’interno, centrata, disponiamo un’immagine qualunque ed un button di chiusura.
+:roccia: MILESTONE 2
+Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
+Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+:roccia: MILESTONE 3
+Inseriamo il pezzo di logica finale: quando una foto viene cliccata, dobbiamo fare in modo che sia proprio quella foto a essere mostrata all’interno dell’overlay.
+Ci sono diversi modi di farlo, prova a sperimentare :faccia_leggermente_sorridente:
+:regalo: BONUS
+Spostandosi col mouse sopra le foto, queste si zoommano, ruotano di 10 gradi e la loro ombra aumenta, il tutto in manierà fluida. Inoltre il mouse diventa un puntatore, per far capire all’utente che può cliccare
--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 // dischiaro const che conterrà le chiavi valore della chiamata api
 const newGroup = []
 
+// effettuo chiamata api, estrapolando il contenuto nell array newGropu
+axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then((resp) =>{
+    resp.data.map(item => {
+        newGroup.push(item)
+        render(newGroup);
+    });
+})
+console.log(newGroup);
 
 // creo la mia funzione per destrutturare e creare una singola card
 const generateCard = (element) => {
-    // creo una stringa dove inserire la mia card
+// creo una stringa dove inserire la mia card
     const card = `<div class="col-12 col-md-6 col-lg-4 mb-5 colonna">
-                        <div class="picSquare">
+                        <div class="cardInfo">
                             <figure>
-                                <img src="${element.url}" talt="" onclick="on()">
+                                <img src="${element.url}" alt="" onclick="on(${element.id})">
                                 <img src="./img/pin.svg" class="point" alt="puntina">
                             </figure>
                             <figcaption>
                                 <div class="dataCard">
-                                    ${element.date}                            
+                                    ${element.date}   
                                 </div>
                                 <div class="titoloCard">
                                     <strong>
-                                        ${element.title}                            
+                                        ${element.title}
                                     </strong>
                                 </div>
                             </figcaption>
@@ -29,7 +37,7 @@ const generateCard = (element) => {
 
 // funzione che mi renderizza il contenuto dell'array
 const render = (array) => {
-    // dichiaro una variabile con valore stringa nuova concatenando le colonne come stringhe
+// dichiaro una variabile con valore stringa nuova concatenando le colonne come stringhe
     let cards = "";
     for(let i=0; i<array.length; i++){
       cards += generateCard(array[i])
@@ -37,24 +45,22 @@ const render = (array) => {
   
     document.getElementById(`content`).innerHTML = cards;
   }
-  
-  // chiamo la funzione
-  
-  // effettuo chiamata AudioParam, estrapolando il contenuto nell array newGropu
-  axios.get(`https://lanciweb.github.io/demo/api/pictures/`).then((resp) =>{
-      resp.data.map(item => {
-          newGroup.push(item)
-          render(newGroup);
-      });
-  })
-  console.log(newGroup);
 
-//   overlay
 
-  function on() {
-    document.getElementById("overlay").style.display = "block";
-  }
+  ///////////////////////////////////   overlay   ///////////////////////////////////
   
+//   recuperò l'elemento dove inseririre le immagine
+  const OverlayImg = document.getElementById(`OverlayImg`);
+  
+// funzione di spegnimento
   function off() {
-    document.getElementById("overlay").style.display = "none";
-  }
+      document.getElementById("overlay").style.display = "none";
+    }
+// funzione che al click dell'immagine,rende visibile l'overlay e trova l'immagine appena cliccata e la apre in overlay
+    function on(id) {
+      document.getElementById("overlay").style.display = "block";
+// Trova l'oggetto corrispondente nell'array
+      const imgSelected = newGroup.find(item => item.id === id);
+// Aggiungi l'immagine all'overla
+          OverlayImg.innerHTML = `<img src="${imgSelected.url}" alt=""width="400" height="auto">>`;
+      }
